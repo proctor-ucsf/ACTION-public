d28475d82e73f37f971afa3ad4a31bccf9a7da9a
Fanice <mangoja@berkeley.edu>
2021-07-06 14:15:36 -0700

    Copy over code and codebooks from main ACTION project

---
+++ code/config.R
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# @Organization - F.I. Proctor Foundation
+# @Project - ACTION primary analysis
+# @Author - Fanice Nyatigo (fanice.nyatigo@ucsf.edu)
+# @Description - configuration file
+#-------------------------------------------------------------------------------
+
+#load packages
+library(tidyverse)
+library(magrittr)
+library(here)
+library(knitr)
+library(kableExtra)
+library(expss)
+library(table1)
+library(foreach)
+library(coin)
+library(broom)
+library(codebook)
+library(labelled)
+library(sqldf)
+library(tmle)
